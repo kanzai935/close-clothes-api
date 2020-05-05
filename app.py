0b081b11c65e1240b1ed9edb06467cc8a6098864
anzai.k <anzai.k@opst.co.jp>
2020-05-05 22:03:17 +0900

    add mongodb

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 from flask import Flask
 from redis import Redis
-
+from pymongo import MongoClient
+from datetime import datetime
+import uuid
+import os
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 
+class User(object):
+
+    def __init__(self):
+        mongodb_username = os.environ['MONGODB_USERNAME']
+        mongodb_password = os.environ['MONGODB_PASSWORD']
+        mongodb_host = os.environ['MONGODB_HOSTNAME']
+        self.client = MongoClient(mongodb_host, 27017)
+        self.db = self.client['close-clothes']
+        self.db.authenticate(mongodb_username,mongodb_password)
+
+    def add_one(self):
+        post = {
+            'user_id': str(uuid.uuid4()),
+            'created_at': datetime.now()
+        }
+        return self.db.users.insert_one(post)
+
+
 @app.route('/')
 def hello():
     redis.incr('hits')
+    user = User()
+    rest = user.add_one()
     return 'Hello World! I have been seen %s times.' % redis.get('hits')
 
 
