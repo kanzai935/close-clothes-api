8b7fe86c6666184ef4f877119c86dac97eddb01d
anzai.k <anzai.k@opst.co.jp>
2020-05-08 12:26:18 +0900

    set cookie and fetch user_id from mongodb

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import os
+from datetime import datetime
+
+from flask import Flask, make_response
 
 from app import api
 
@@ -9,5 +12,21 @@ def create_app(env):
     @app.route('/')
     def index():
         return api.index()
+
+    @app.route('/user/add', methods=['GET'])
+    def add_user():
+        user_id = api.add_user()
+        response = make_response()
+        max_age = 60
+        domain = os.environ['DOMAIN_NAME']
+        expires = int(datetime.now().timestamp()) + max_age
+        response.set_cookie('user_id', value=user_id, max_age=max_age, expires=expires, path='/*', domain=domain,
+                            secure=None, httponly=True)
+        return response
+
+    @app.route('/user', methods=['GET'])
+    def fetch_user():
+        # user_id = request.cookies.get('user_id')
+        return api.fetch_user('f649d16b-854d-41ae-b900-60aa1373692f')
 
     return app
--- app/api.py
+++ app/api.py
@@ -6,29 +6,44 @@ from pymongo import MongoClient
 from redis import Redis
 
 redis = Redis(host='redis', port=6379)
+mongodb_username = os.environ['MONGODB_USERNAME']
+mongodb_password = os.environ['MONGODB_PASSWORD']
+mongodb_host = os.environ['MONGODB_HOST']
+mongodb_database_name = os.environ['MONGODB_DATABASE_NAME']
 
 
 class User(object):
 
     def __init__(self):
-        mongodb_username = os.environ['MONGODB_USERNAME']
-        mongodb_password = os.environ['MONGODB_PASSWORD']
-        mongodb_host = os.environ['MONGODB_HOSTNAME']
-        mongodb_database = os.environ['MONGODB_DATABASE']
-        self.client = MongoClient(mongodb_host, 27017)
-        self.db = self.client[mongodb_database]
-        self.db.authenticate(mongodb_username, mongodb_password)
+        self.mongodb_db = MongoClient(mongodb_host, 27017)[mongodb_database_name]
+        self.mongodb_db.authenticate(mongodb_username, mongodb_password)
 
     def add_one(self):
+        user_id = str(uuid.uuid4())
         post = {
-            'user_id': str(uuid.uuid4()),
+            'user_id': user_id,
             'created_at': datetime.now()
         }
-        return self.db.users.insert_one(post)
+        self.mongodb_db.users.insert_one(post)
+        return user_id
+
+    def fetch_one(self, user_id):
+        user = self.mongodb_db.users.find_one({'user_id': user_id})
+        return user['user_id']
 
 
 def index():
     redis.incr('hits')
-    user = User()
-    rest = user.add_one()
     return 'Hello World! I have been seen %s times.' % redis.get('hits')
+
+
+def add_user():
+    user = User()
+    user_id = user.add_one()
+    return user_id
+
+
+def fetch_user(user_id):
+    user = User()
+    result_user_id = user.fetch_one(user_id)
+    return result_user_id
